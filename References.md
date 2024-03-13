0cf7142c8da78f9d5879c9d8ae270b7a19947a1f
msli5 <162515271+msli5@users.noreply.github.com>
2024-03-13 22:52:43 +0000

    Initial commit

---
+++ References.md
@@ -0,0 +1,3 @@
+P5's Reference: https://p5js.org/reference/
+
+Ms. Li's Reference:https://bit.ly/p5-reference
---
+++ script.js
@@ -0,0 +1,22 @@
+function setup() {
+  // Code here runs only once
+  createCanvas(800, 600);
+}
+
+function draw() {
+  // Code here runs continuously
+  background(255);
+
+  //drawGrid() is referenced to the drawGrid.js file. You can hide the grid by adding "//" in front of it. If you want to see the grid OVER your sketch, move it to the line before }.
+  drawGrid()
+  stroke(0)
+
+
+
+
+
+
+
+  
+
+}
